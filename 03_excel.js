0a85d11bb31e057abb2d1b27f14c9946fcbb3014
Yoichi Watanabe <chip.star1120@gmail.com>
2017-03-12 16:41:55 +0900

    add sort function

--- 03_excel.js
+++ 03_excel.js
@@ -16,17 +16,56 @@ var Excel = React.createClass({
     displayName: 'Excel',
     
     getInitialState:function() {
-        return {data: this.props.initialData};
+        return {
+            data: this.props.initialData,
+            sotrby: null,
+            descending: false,
+        };
+    },
+
+    propTypes: {
+        headers: React.PropTypes.arrayOf(
+            React.PropTypes.string
+        ),
+        initialData: React.PropTypes.arrayOf(
+            React.PropTypes.arrayOf(
+                React.PropTypes.any
+            )
+        ),
+    },
+
+    _sort: function(e) {
+        var column = e.target.cellIndex;
+        var descending = 
+            this.state.sortby === column 
+            && !this.state.descending;
+        var data = this.state.data.slice();
+
+        data.sort(function(a, b) {
+            //return a[column] > b[column] ? 1 : -1;
+            return descending
+                ? (a[column] < b[column] ? 1 : -1)
+                : (a[column] > b[column] ? 1 : -1);
+        });
+
+        this.setState({
+            data: data,
+            sortby: column,
+            descending: descending,
+        });
     },
 
     render: function() {
         return (
             React.DOM.table(null,
-                React.DOM.thead(null,
+                React.DOM.thead({onClick: this._sort},
                     React.DOM.tr(null,
                         this.props.headers.map(function(title, idx) {
+                            if (this.state.sortby === idx) {
+                                title += this.state.descending ? ' \u2191' : ' \u2193'
+                            }
                             return React.DOM.th({key: idx}, title);
-                        })
+                        }, this)
                     )
                 ),
                 React.DOM.tbody(null,
