30e01899b39797da15f26cde93f74f8f56670f00
Yoichi Watanabe <chip.star1120@gmail.com>
2017-03-20 16:39:18 +0900

    add Button component

---
+++ whinepad/js/source/components/Button.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import React, {PropTypes} from 'react';
+
+const Button = props => {
+    const cssclasses = classNames('Button', props.className);
+    return props.href
+    ? <a {...props} className={cssclasses} />
+    : <button {...props} className={cssclasses} />;
+};
+
+ /*
+function Button(props) {
+    const cssclasses = classNames('Button', props.className);
+    return props.href
+    ? <a {...props} className={cssclasses} />
+    : <button {...props} className={cssclasses} />;
+}*/
+
+Button.propTypes = {
+        href: PropTypes.string,
+};
+
+export default Button
